b519d441e90eadd027de0a540705abd0af06e373
Julia243b <julmota38@gmail.com>
2025-08-28 23:07:22 -0300

    Arquivos JS - tipos de dados e sintaxe geral

---
+++ js/00-intro.js
@@ -0,0 +1,42 @@
+ // Comentário JavaScript
+
+// Tag do fechamento do body <script><script> elementos do js
+       
+//alert("Olá mundo!");
+        
+/*
+    É uma linguaguem de programação interpretada
+    É uma linguaguem dinâmica
+*/
+
+/* declarando uma variável - var (metodo antigo)
+    Não se usa mais esse tipo de declaração com var */
+var nome = "Julia";
+
+
+// Let e const - declaração camelCase - caso do camelo
+
+// Let é o modo mais moderno de lidar com variaveís
+// Case-sensitive - significa que letras maiúsculas e minúsculas são tratadas como diferentes.
+let nomeDoAluno = "Mago";
+nomeDoAluno = "Tenis";
+let nomedoAluno = "Jinu";
+let NOMEDOALUNO = "Mira";
+
+
+// console.log(nomeDoAluno)
+        
+// const (constante) - Declarando variavel do tipo constante
+const idade = 20;
+
+
+// redeclaração de variavel - não precisa do escopo de declaração (var, lest, const)
+// nomeDoAluno = "Paulo"
+// idade = 41
+
+
+let valor = "teste";
+console.log(typeof valor);
+        
+valor = 20.987123;
+console.log(typeof valor);
