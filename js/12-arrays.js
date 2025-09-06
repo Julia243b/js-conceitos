1310c6e5aa3a57707ca86f48afcd53a2866a1010
Julia243b <julmota38@gmail.com>
2025-09-06 12:48:49 -0300

    Sobre listas arrays

---
+++ js/12-arrays.js
@@ -0,0 +1,11 @@
+// Arrays representam listas ordenadas de valores.
+// Pensa nele como uma prateleira numerada em que cada espaço(posição) pode guardar algum tipo de dado.
+//os arrays são representados por []
+
+// a ideia de lista ordenada é por conta da ordem númerica que os elementos ocupam. Geralmente o primeiro elemento está na posição/indice 0.
+
+let frutas = ["Maçã", "Banana", "Laranja"];
+
+// let fruta1 = "Maça";
+// let fruta2 = "Banana";
+// let fruta3 = "Laranja";
